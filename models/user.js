13967282c675606061bb01e76d75f30c14db88d6
SALAH MATHLOUTHI <147411955+salahmath@users.noreply.github.com>
2025-01-11 19:44:25 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "client"], default: "client" },
-  phone: { type: String },
+  phone: { type: String, required: true },
   isBlocked: { type: Boolean, default: false},
   birth:{type: Date}
 }, { timestamps: true });
